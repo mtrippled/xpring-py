c101e09981f711ef69a06f35ffa19e788ffa3418
Mark Travis <mtravis@ripple.com>
2022-05-19 09:15:23 -0700

    Test transaction for trying to update the serializer.

--- tests/test_definitions.py
+++ tests/test_definitions.py
@@ -19,11 +19,23 @@ TRANSACTION_EXAMPLES = [
             'Flags': 9,
             'URI': '4e4654206d696e742074657374',
         },
+        {
+            'TransactionType': 'NFTokenCreateOffer',
+            'Account': 'rpquDbJEKHLnecncaEXGYnKHaNNksAcV8j',
+            'Owner': 'rJYJSYY8Uz699JEUmsNHkkyW89kHJFb2yR',
+            'Fee': '12',
+            'NFTokenID': '00090001C0659777390FF0082067917C690C222E7D07CAF0F7BFFFB100000016',
+            'Amount': '5',
+            'Flags': 0,
+            'Sequence': 70638780
+        },
     )
 ]
 # yapf: enable
 
-@pytest.mark.parametrize(('transaction',), TRANSACTION_EXAMPLES)
-def test_serialize_transaction(transaction):
+@pytest.mark.parametrize(('transaction','transaction2'), TRANSACTION_EXAMPLES)
+def test_serialize_transaction(transaction, transaction2):
     blob = serialization.serialize_transaction(transaction)
+    assert blob.hex()
+    blob = serialization.serialize_transaction(transaction2)
     assert blob.hex()
